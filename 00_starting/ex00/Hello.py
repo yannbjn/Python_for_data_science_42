6688366148eb720028a6abe675ed76966ef11d57
frite <frite@42paris.fr>
2025-11-20 17:27:48 +0100

    start ex00 of Starting module

---
+++ 00_starting/ex00/Hello.py
@@ -0,0 +1,18 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple    = ("Hello", "toto!")
+ft_set  = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+# ft_list.remove("tata!")
+# ft_list.append('World')
+
+#easy way bcs I'm a dumbass
+ft_list[1] = "World!"
+ft_tuple = ("Hello", "France!")
+
+ft_set.discard("tutu!")
+ft_set.add("Paris!")
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
